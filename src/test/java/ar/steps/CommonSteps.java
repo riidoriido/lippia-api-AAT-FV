6eb388dfbe9c66527074492495e2df6205cf1804
Fermin Vazquez <fervazquez.dev@gmail.com>
2024-06-27 17:13:55 -0300

    feature, models, y jsons

--- src/test/java/ar/steps/CommonSteps.java
+++ src/test/java/ar/steps/CommonSteps.java
@@ -1,15 +1,11 @@
 package ar.steps;
 
 import api.config.EntityConfiguration;
-import api.model.GetTimeEntry;
 import com.crowdar.core.PageSteps;
 import com.google.api.client.repackaged.com.google.common.base.Splitter;
 import io.cucumber.java.en.*;
 import org.apache.commons.lang.StringUtils;
-import services.BaseService;
-import services.TimeEntryGET;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
